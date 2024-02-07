8d3c11e9747f85e81443d46cd5ebdbfd5260f0a2
LuizG <luiz.mwlo76@gmail.com>
2024-02-07 11:26:01 -0300

    javascript - básico

---
+++ 01-variaveis.js
@@ -0,0 +1,11 @@
+//criando variáveis com "var" (var nome_variavel = <valor>;)
+
+var nome = "Luiz"; //var: cria variável global
+var idade = 16;
+var info = console.log("Olá, meu nome é", nome, "e tenho", idade, "anos!");
+console.log("Fiz aniversário! Agora tenho", idade + 1, "anos!");
+
+// aumentando automágicamente a idade (sem ser manual)
+
+idade++ //idade = idade + 1;
+console.log("Fiz aniversário! Agora tenho", idade, "anos!");
---
+++ 02-aritimetico.js
@@ -0,0 +1,29 @@
+// Utilizando operadores aritiméticom com JavaScript
+
+console.log("A soma entre 8 e 7 é", 8 + 7);
+
+var x = 20;
+var y = 10;
+// efetuar a soma de 2 números informados por mim: ex(var nome_variavel = 12)
+
+console.log("soma de", x, "+", y, "é igual a:", x + y);
+
+var adicao = x + y;
+var subtracao = x - y;
+var divisao = x / y;
+var multiplicacao = x * y;
+
+console.log("")
+
+console.log("soma = ", adicao);
+console.log("subtracao =", adicao);
+console.log("divisao =", divisao);
+console.log("multiplicacao =", multiplicacao);
+
+// regras de nomeação de identificadores no NOTION.
+// podem começar com a-z, A-Z, _ ou $
+
+var MinhaVariavel = 0
+var OutraVariavel = 20
+const _ConstanteEpica = 3.151427
+var $uperVariavel = 1.61
