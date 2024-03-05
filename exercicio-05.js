65b7f324e5ba957a460ca943fe2ea7edcba4610f
LuizG <luiz.mwlo76@gmail.com>
2024-03-05 07:59:23 -0300

    tirei o comentario

--- exercicio-05.js
+++ exercicio-05.js
@@ -1,31 +1,31 @@
-// function idade(anoNascimento) {
-//   return anoAtual - anoNascimento;
-// }
+function idade(anoNascimento) {
+  return anoAtual - anoNascimento;
+}
 
-// var nascimento = 2007;
-// var anoAtual = 2024;
+var nascimento = 2007;
+var anoAtual = 2024;
 
-// console.log(
-//   `Você nasceu em ${nascimento}, logo, você tem ${idade(nascimento)} anos!`
-// );
+console.log(
+  `Você nasceu em ${nascimento}, logo, você tem ${idade(nascimento)} anos!`
+);
 
-// console.log(` `);
+console.log(` `);
 
-// console.log(`----------------------------------------------------------------`);
+console.log(`----------------------------------------------------------------`);
 
-// function distViagem(quantCombustivel) {
-//   return quantCombustivel * 12;
-// }
+function distViagem(quantCombustivel) {
+  return quantCombustivel * 12;
+}
 
-// var combustivel = 30;
+var combustivel = 30;
 
-// console.log(
-//   `Você possui ${combustivel} Litros de combustível. Você poderá percorrer por ${distViagem(
-//     combustivel
-//   )} Kilômetros.`
-// );
-// console.log(" ");
-// console.log("-------------------------------------------------------");
+console.log(
+  `Você possui ${combustivel} Litros de combustível. Você poderá percorrer por ${distViagem(
+    combustivel
+  )} Kilômetros.`
+);
+console.log(" ");
+console.log("-------------------------------------------------------");
 
 var idadeVoto = 16;
 
